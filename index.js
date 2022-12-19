6a3a1f2c9341cdfa4b84f4ee977e194441a6e280
rachitabrol <61756984+rachitabrol@users.noreply.github.com>
2022-12-19 17:24:16 +0530

    completed microservice

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+var dns = require('dns');
+const URL = require('url').URL;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,6 +13,10 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+//middleware
+app.use(bodyParser.urlencoded({extended:false})) //contains more than strings i.e. here it accepts objects otherwise won't be found
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -17,6 +24,29 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+let hashmap = new Map();
+let counter = 1;
+app.post('/api/shorturl',(req,res)=>{
+  const og = req.body.url;
+  const oog = new URL(og);
+  //check whether url is valid
+  dns.lookup(oog.hostname,function (err, addresses, family) {
+    if(err){
+    console.log("TIME : ",new Date()," ERROR : ",err);
+    res.json({ error: 'invalid url' });
+    }else{
+      //shortern it
+      hashmap.set(counter,og);
+      res.json({original_url:og,short_url:counter++});
+    }
+  });
+  
+  
+})
+
+app.get('/api/shorturl/:surl',(req,res)=>{
+  res.redirect(hashmap.get(parseInt(req.params.surl)));
 });
 
 app.listen(port, function() {
